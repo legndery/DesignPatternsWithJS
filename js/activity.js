a1f9087a5235918d6e2b06a650fe34c763392d51
legndery <ar_itra@yahoo.com>
2017-08-31 02:16:52 +0530

    added font and js change

--- js/activity.js
+++ js/activity.js
@@ -1,7 +1,7 @@
 
 let Activity = (function(){
 
-    let activities= [];
+    let activities= [{"time":"2017-08-29 17:08","activity":"afafaf"}];
     
     //bind ui events
 
@@ -10,27 +10,54 @@ let Activity = (function(){
     const $input = $app.find('input.activity-text');
     const $goButton = $app.find('button.go-btn');
     const $activityList = $app.find('ul.activity-list');
-    const $template = $('<li />', {"class": "item"})
+    const template = $app.find('#list-item-template').html();
 
     //bindUI
     $goButton.on('click', addActivity)
+    _render()
+
 
     //render
-    var _render = () => {
+    function _render(){
         $activityList.empty()
         for( let {activity:a, time:t} of activities){
-            $activityList.append($template.clone().text(`${t}: I am doing ${a}`));
+            $activityList.append($(template).clone().prepend(`${t}: I am ${a}`));
         }
     }
-
+    
+    const _getDate = (d)=>{
+        function pad(number) {
+            if (number < 10) {
+              return '0' + number;
+            }
+            return number;
+          }
+          return d.getUTCFullYear() +
+          '-' + pad(d.getUTCMonth() + 1) +
+          '-' + pad(d.getUTCDate()) +
+          ' ' + pad(d.getUTCHours()) +
+          ':' + pad(d.getUTCMinutes());
+    }
     //methods
-    function addActivity(v) {
-        const activity = (typeof v == "string")?v:$input.val();
-        const time = (new Date()).toISOString();
+    function addActivity(e) {
+        const activity = (typeof e == "string")?e:$input.val();
+        const time= _getDate(new Date());
+                
         activities.push({activity, time});
         _render();
     }
+    function removeActivity(e){
+        let i;
+        if(typeof e == "number"){
+            i = e;
+        }else{
+            $remove = $(e.target).closest('li');
+            i = $activityList.find('li').index($remove);
+        }
+        activities.slice(i,1);
+        _render();
 
+    }
     return {
         addActivity
     }
